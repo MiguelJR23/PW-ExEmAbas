a757975a60567e1abe555d46eeaa11158042fce1
MiguelJR23 <r0b3rts2332@gmail.com>
2026-05-15 20:55:00 -0300

    Add exercise 1: Tabuada with HTML form

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const exercises = [
+
+{
+title: "1. Tabuada",
+html: `
+<h2>Tabuada</h2>
+
+<form action="processar.php" method="POST">
+    <input type="hidden" name="exercicio" value="1">
+
+    <input type="number" name="numero" placeholder="Digite um número">
+
+    <button type="submit">Calcular</button>
+</form>
+`
+},
