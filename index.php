a0b1d6da076a0095a812b569b14f764b4d63a99d
MiguelJR23 <r0b3rts2332@gmail.com>
2026-05-15 20:49:55 -0300

    Add initial HTML structure to index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-PW</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js" defer></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="tabs" id="tabBar"></div>
+    <div class="content" id="contentArea"></div>
+</div>
+
+</body>
+</html>
