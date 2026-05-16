539752f679f2db21c67a28cf204f19b491000f4a
MiguelJR23 <r0b3rts2332@gmail.com>
2026-05-16 19:51:02 -0300

    2 & 3

--- js/script.js
+++ js/script.js
@@ -14,3 +14,42 @@ html: `
 </form>
 `
 },
+
+{
+title: "2. Desconto",
+html: `
+<h2>Calcular desconto</h2>
+
+<form action="processar.php" method="POST">
+
+    <input type="hidden" name="exercicio" value="2">
+
+    <input type="number" step="0.01" name="preco" placeholder="Preço">
+
+    <input type="number" step="0.01" name="desconto" placeholder="Desconto(%)">
+
+    <button type="submit">Calcular</button>
+
+</form>
+`
+},
+
+{
+title: "3. Média de 4 notas",
+html: `
+<h2>Verificar aprovação</h2>
+
+<form action="processar.php" method="POST">
+
+    <input type="hidden" name="exercicio" value="3">
+
+    <input type="number" step="0.1" name="n1" placeholder="Nota 1">
+    <input type="number" step="0.1" name="n2" placeholder="Nota 2">
+    <input type="number" step="0.1" name="n3" placeholder="Nota 3">
+    <input type="number" step="0.1" name="n4" placeholder="Nota 4">
+
+    <button type="submit">Verificar</button>
+
+</form>
+`
+},
